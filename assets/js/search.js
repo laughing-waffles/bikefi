769ca11e89e201cf23b70b6fdbd43b279aba5f8f
Ed Ireson <ed@ireson.net>
2022-03-03 16:40:04 -0600

    Update search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,31 +1,18 @@
-$(document).ready(function(){
+const searchClient = algoliasearch('UF8CKBPQYZ', '2f15cc520d959080cb0333d11eb89d59');
+
 const search = instantsearch({
-    appId: 'R6Y1H83FNX',
-    apiKey: 'b0f283176b810e5223f70e3ab3ac7c04',
-    indexName: 'albums',
-    searchClient: algoliasearch(
-        'B7Y6H7ORZK',
-        'd891dce922face3261d109172600d2cc'
-      ),
-    routing: true
-  });
-  
-  search.addWidget(
-    instantsearch.widgets.searchBox({
-      container: '#search-box',
-      placeholder: 'Search for albums'
-    })
-  );
-  
-  search.addWidget(
-    instantsearch.widgets.hits({
-      container: '#hits',
-      templates: {
-        empty: 'No results',
-        item: '<em>Hit {{objectID}}</em>: {{{_highlightResult.content.value}}}'
-      }
-    })
-  );
-  
-  search.start();
+  indexName: 'financing.bike',
+  searchClient,
 });
+
+search.addWidgets([
+  instantsearch.widgets.searchBox({
+    container: '#search-box',
+  }),
+
+  instantsearch.widgets.hits({
+    container: '#hits',
+  })
+]);
+
+search.start();
