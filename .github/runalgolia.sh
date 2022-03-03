d328dfc69837834a4ce0da4016030c238743138a
Ed Ireson <ed@ireson.net>
2022-03-03 09:51:53 -0600

    alg2

--- .github/runalgolia.sh
+++ .github/runalgolia.sh
@@ -1,3 +1,3 @@
 echo 'Starting Jekyll build'
 chmod -R a+w /github/workspace
-ALGOLIA_API_KEY='${{ secrets.ALGOLIA_API_KEY }}' bundle exec jekyll algolia
+ALGOLIA_API_KEY='$1' bundle exec jekyll algolia
