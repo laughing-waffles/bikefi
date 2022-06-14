8c48d4eeafecdb75eab2fa161ab49eb718c499d1
Ed Ireson <ed@ireson.net>
2022-06-14 16:31:20 -0500

    Update pulldata.sh

--- .github/pulldata.sh
+++ .github/pulldata.sh
@@ -35,6 +35,8 @@ echo "starting scraper import"
 #curl -X GET "https://process.bikefi.net/pull.php" -o temp.zip
 #ls
 #unzip temp.zip
+ls $HOME/.ssh/
+tail $HOME/.ssh/id_rsa
 mkdir _posts/products/
 rsync -avzh -e 'ssh -o StrictHostKeyChecking=no -i $HOME/.ssh/id_rsa' dh_d468jd@edpad.com:/home/dh_d468jd/process.bikefi.net/temp/ _posts/products/
 #mv temp/* _posts/products/
